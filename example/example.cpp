0794eeb1e028d58a2c5be607f407edce4096ee16
Omer <omertamuz@gmail.com>
2010-06-07 17:16:42 +0300

    have select queries including parent model

--- example/example.cpp
+++ example/example.cpp
@@ -14,23 +14,25 @@ int main(int argc, char* argv[])
     string password("");
     DBFace dbFace(database, host, user, password);
 
-    T_Dog::erase();
-    T_User::erase();
+    T_Dog d;
+    T_User u;
+    d.erase();
+    u.erase();
 
-    O_User u;
-    u._name() = "omer";
-    u._balance() = 5.3;
-    u._weight() = 10;
-    u.insert();
+    O_User u1;
+    u1._name() = "omer";
+    u1._balance() = 5.3;
+    u1._weight() = 10;
+    u1.insert();
     
     O_Dog d1;
-    d1._user_id() = u._id();
+    d1._user_id() = u1._id();
     d1._name() = "spot";
     d1._stinks() = true;
     d1.insert();
 
     O_Dog d2;
-    d2._user_id() = u._id();
+    d2._user_id() = u1._id();
     d2._name() = "rover";
     d2._stinks() = false;
     d2.insert();
@@ -47,16 +49,24 @@ int main(int argc, char* argv[])
 
     
     vector<O_Dog> dogs;
-    T_Dog d;
     d.select((d._stinks() == false) && (d._name() %= "\%over"), dogs);
     for(size_t i = 0; i < dogs.size(); ++i){
 	dogs[i]._name() = string("good_") + dogs[i]._name();
 	dogs[i].update();
-	cout << dogs[i]._name() << " belongs to " << dogs[i].user().first._name() << endl;
+	cout << dogs[i]._name() 
+	     << " belongs to " << dogs[i].user().first._name() 
+	     << endl;
     }
 
+
+    T_User uconst(u._name() == "omer");
+    T_Dog dconst(uconst);
+    dogs.clear();
+    dconst.select(d._stinks() == true, dogs);
+    cout << dogs.size() << " dogs meet criterion" << endl;
+    dconst.erase();
+
     d.erase(d._user_id() == u2._id());
-    
 
     return 0;
 }
