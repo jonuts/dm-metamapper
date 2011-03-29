709a1ea7d9b2d528ecf168d28048de5023a03c72
Omer <omertamuz@gmail.com>
2011-03-29 12:22:46 +0200

    reconnecting to DB if disconnected

--- lib/dm-metamapper/version.rb
+++ lib/dm-metamapper/version.rb
@@ -1,6 +1,6 @@
 module DataMapper 
   module MetaMapper
-    VERSION = "0.1.5" unless defined?(::DataMapper::MetaMapper::VERSION)
+    VERSION = "0.1.6" unless defined?(::DataMapper::MetaMapper::VERSION)
   end
   
 end
