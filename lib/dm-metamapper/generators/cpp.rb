dcb8a87739d4558679440441a06ac8e6c43baa7b
Omer <omertamuz@gmail.com>
2012-01-31 13:48:20 +0200

    added get children functionality

--- lib/dm-metamapper/generators/cpp.rb
+++ lib/dm-metamapper/generators/cpp.rb
@@ -17,10 +17,6 @@ module DataMapper
         generates_file :model, "T_%model%.hpp", :template => "class.hpp"
 
         setup_model do
-          key_to_parent = many_to_one.inject({}) do |hash, (r,m)|
-            hash[m.child_key.first.name] = decolonize(m.parent_model_name.to_const_string)
-            hash
-          end
           enums = {}
           model.properties.each do |prop|
             cpp_name = if prop.serial?
@@ -40,6 +36,19 @@ module DataMapper
           end
           (class << model; self; end).instance_eval{ attr_accessor :enums }
           model.enums = enums
+        end
+        
+        def key_to_parent
+          many_to_one.inject({}) do |hash, (r,m)|
+            hash[m.child_key.first.name] = decolonize(m.parent_model_name.to_const_string)
+            hash
+          end
+        end
+
+        def child_key(child_model)
+          child_model.relationships.select {|m|
+            m.class.name == 'DataMapper::Associations::ManyToOne::Relationship' && m.parent_model_name == model.name
+          }.first.child_key.first.name.to_s
         end
 
         def output_path(model, template)
@@ -66,6 +75,20 @@ module DataMapper
           end
 
           @many_to_one
+        end
+        def one_to_many
+          return unless model
+          @one_to_many ||= model.relationships.select {|m|
+            m.class.name == 'DataMapper::Associations::OneToMany::Relationship'
+          }
+          #in case of ruby < 1.9
+          if Array === @one_to_many
+            temp = @one_to_many
+            @one_to_many = {}
+            temp.each{|t| @one_to_many[t.name] = t}
+          end
+
+          @one_to_many
         end
       end
     end
