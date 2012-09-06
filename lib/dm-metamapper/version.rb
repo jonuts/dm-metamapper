aa11fe7be741ded9f4fa1b469e3ca350e0640c3f
Omer <omertamuz@gmail.com>
2012-09-06 11:38:20 +0300

    bump to 0.3

--- lib/dm-metamapper/version.rb
+++ lib/dm-metamapper/version.rb
@@ -1,6 +1,6 @@
 module DataMapper 
   module MetaMapper
-    VERSION = "0.2.9" unless defined?(::DataMapper::MetaMapper::VERSION)
+    VERSION = "0.3" unless defined?(::DataMapper::MetaMapper::VERSION)
   end
   
 end
