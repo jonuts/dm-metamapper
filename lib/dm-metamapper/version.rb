f3983856902578667b73afd95b62f19a5ddd4203
Omer <omertamuz@gmail.com>
2012-09-24 11:11:17 +0200

    shorter log file lines

--- lib/dm-metamapper/version.rb
+++ lib/dm-metamapper/version.rb
@@ -1,6 +1,6 @@
 module DataMapper 
   module MetaMapper
-    VERSION = "0.3.1" unless defined?(::DataMapper::MetaMapper::VERSION)
+    VERSION = "0.3.2" unless defined?(::DataMapper::MetaMapper::VERSION)
   end
   
 end
