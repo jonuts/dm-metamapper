7d527a9ee6782d62f6eac59186f572e7316c71df
Ng Tze Yang <ngty77@gmail.com>
2014-11-05 22:24:05 +0800

    Looks like we dont need dm-core.

--- lib/dm-metamapper.rb
+++ lib/dm-metamapper.rb
@@ -1,13 +1,13 @@
 lib = File.dirname(__FILE__)
 $:.push lib unless $:.include?(lib)
 
-require 'dm-core'
 
 require 'metamapper/metamapper'
 require 'metamapper/template'
 require 'metamapper/generator'
 
 =begin
+require 'dm-core'
 require 'dm-metamapper/property'
 require 'dm-metamapper/model'
 require 'dm-metamapper/config'
